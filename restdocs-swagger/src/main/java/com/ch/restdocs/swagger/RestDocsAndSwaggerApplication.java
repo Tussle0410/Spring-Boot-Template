1c33bef13d62effb113e4ec5c898f257f929093b
chanhyeonglee <ok.lee3@okestro.com>
2025-02-07 15:40:33 +0900

    [Feature] RestDocs + Swagger Boot Application 추가

--- restdocs-swagger/src/main/java/com/ch/restdocs/swagger/RestDocsAndSwaggerApplication.java
+++ restdocs-swagger/src/main/java/com/ch/restdocs/swagger/RestDocsAndSwaggerApplication.java
@@ -1,5 +1,12 @@
 package com.ch.restdocs.swagger;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class RestDocsAndSwaggerApplication {
 
+  public static void main(String[] args) {
+    SpringApplication.run(RestDocsAndSwaggerApplication.class, args);
+  }
 }
